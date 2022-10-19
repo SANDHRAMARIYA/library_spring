2dd7da86e9640cd29ea1f6d51bec3c78db09567a
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-19 22:23:33 +0530

    mastr

---
+++ src/main/java/com/nestdigital/library_manag/LibraryManagApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.library_manag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/library_manag/LibraryManagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.library_manag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

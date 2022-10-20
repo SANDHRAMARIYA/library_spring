ad1e302f8f0b35781daa5041b533676887ad48ef
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-20 14:33:43 +0530

    dev

--- src/main/java/com/nestdigital/library_manag/Controller/LibraryController.java
+++ src/main/java/com/nestdigital/library_manag/Controller/LibraryController.java
@@ -5,6 +5,7 @@ import com.nestdigital.library_manag.Model.LibraryModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -14,8 +15,8 @@ public class LibraryController {
 
 
     @CrossOrigin(origins = "*")
-    @PostMapping(path = "/addlibrary",consumes = "application/json",produces = "application/json")
-    public String addlibrary(@RequestBody LibraryModel library){
+    @PostMapping(path = "/addlibrary", consumes = "application/json", produces = "application/json")
+    public String addlibrary(@RequestBody LibraryModel library) {
         System.out.println(library.toString());
         dao.save(library);
         return "{status:'success'}";
@@ -23,8 +24,21 @@ public class LibraryController {
 
     @CrossOrigin(origins = "*")
     @GetMapping("/viewlibrary")
-    public List<LibraryModel> viewlibrary()
-    {
-        return  (List<LibraryModel>) dao.findAll();
+    public List<LibraryModel> viewlibrary() {
+        return (List<LibraryModel>) dao.findAll();
+    }
+
+    @Transactional
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/deletelibrary",consumes = "application/json",produces = "application/json")
+    public String deletelibrary(@RequestBody LibraryModel library) {
+        dao.deleteBookById(library.getId());
+        return "{status:'success'}";
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/searchlibrary",consumes = "application/json",produces = "application/json")
+    public List<LibraryModel> searchlibrary(@RequestBody LibraryModel library){
+        return  (List<LibraryModel>) dao.searchlibrary(library.getIssuedate());
     }
 }
--- src/main/java/com/nestdigital/library_manag/Dao/LibraryDao.java
+++ src/main/java/com/nestdigital/library_manag/Dao/LibraryDao.java
@@ -1,7 +1,22 @@
 package com.nestdigital.library_manag.Dao;
 
 import com.nestdigital.library_manag.Model.LibraryModel;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LibraryDao extends CrudRepository <LibraryModel,Integer> {
+
+    @Modifying
+    @Query(value = "DELETE FROM `libraries` WHERE `id`=:id",nativeQuery = true)
+void deleteBookById(Integer id);
+
+    @Query(value = "SELECT `id`, `authorname`, `bookname`, `booktype`, `issuedate` FROM `libraries` WHERE `issuedate`=:issuedate",nativeQuery = true)
+    List<LibraryModel> searchlibrary(String issuedate);
 }
+
+
+
+
